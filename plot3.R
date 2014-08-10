57a3f3aee7b05f26a11de7d7d07271e17a85031b
Alex K <akaluzny@gmail.com>
2014-08-10 18:35:26 -0500

    Bundled with calls together in one line

--- plot3.R
+++ plot3.R
@@ -30,14 +30,14 @@ Plot3 <- function() {
   # Create graphics device for png file
   png(file = "plot3.png")
 
-  # Create a line for sub metering 1
-  with(consumption, plot(datetime, Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l"))
-  
-  # Create a line for sub metering 2
-  with(consumption, lines(datetime, Sub_metering_2, type = "l", col = "red"))
-  
-  # Create a line for sub metering 3
-  with(consumption, lines(datetime, Sub_metering_3, type = "l", col = "blue"))
+  with(consumption, {
+    # Create a line for sub metering 1
+    plot(datetime, Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l")
+    # Create a line for sub metering 2
+    lines(datetime, Sub_metering_2, type = "l", col = "red")
+    # Create a line for sub metering 3
+    lines(datetime, Sub_metering_3, type = "l", col = "blue")
+  })
   
   # Add a legend
   legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1))
